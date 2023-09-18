4ed203dd5b53ab9d92b9d80d92d62aad635f5691
Silisors <gamerpg08@gmail.com>
2023-09-18 09:51:31 -0500

    subí todo en esta rama

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+// app-routing.module.ts
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'post/:id',
+    component: AppComponent
+  },
+  // Otras rutas aquí si es necesario
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'X-Forward-Ip': '192.168.1.7'
+  })
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'apiintegration';
+  id!: string | null;
+
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+
+  ngOnInit() {
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (!this.id) {
+      this.id = 'valor predeterminado';
+    }
+    this.getDatos(this.id);
+  }
+  
+
+  getDatos(id: string) {
+    return this.http.get(`https://general.core.blumerdev.com/post_http/post/${id}`, httpOptions).subscribe(data => {
+      console.log(data); 
+    }, error => {
+      console.error('Ha ocurrido un error:', error); 
+    });
+  }
 }
